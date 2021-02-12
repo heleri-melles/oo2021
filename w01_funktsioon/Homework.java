4a81cd61cdb55c038a56971b19877c3daf67cde2
Heleri Melles <heleri.mellesrgmail.com>
2021-02-12 14:27:11 +0200

    My 1. homework

---
+++ w01_funktsioon/Homework.java
@@ -0,0 +1,9 @@
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println(areaTriangle(5,6));
+    }
+    public static int areaTriangle(int height, int base) {
+        int areaTriangle = (base*height)/2;
+        return areaTriangle;
+    }
+}
