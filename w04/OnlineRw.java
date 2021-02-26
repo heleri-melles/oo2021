ad7ea09ab85b9c6295b526e46811437de0d169d3
Heleri Melles <heleri.mellesrgmail.com>
2021-02-26 14:16:10 +0200

    w4

--- w04/OnlineRw.java
+++ w04/OnlineRw.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 
-public class OnlineRW {
+public class OnlineRw{
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(
       new InputStreamReader(
